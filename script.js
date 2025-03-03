51beb58a56af517e932363788346af1140279cfe
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-03-03 11:57:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,44 +23,71 @@ document.addEventListener("DOMContentLoaded", () => {
     songAudio.src = `Songs/${song}.mp3`;
   }
 
-  function rotatingImg() {
-    if (musicImg.style.webkitAnimationPlayState === "running") {
-      musicImg.style.webkitAnimationPlayState = "paused";
-    } else {
-      musicImg.style.webkitAnimationPlayState = "running";
-    }
-  }
-
   play.addEventListener("click", () => {
-    musicImg.classList.toggle("musicPlaying");
+    musicImg.classList.add("musicPlaying");
+    musicImg.style.webkitAnimationPlayState = "running";
     musicInfo.classList.toggle("playSong");
     pauseActive.classList.toggle("active");
     play.classList.toggle("active");
     songAudio.play();
-    // rotatingImg();
   });
 
   pauseActive.addEventListener("click", () => {
-    // musicImg.style.webkitAnimationPlayState = "paused";
-    musicImg.classList.remove("musicPlaying");
+    musicImg.style.webkitAnimationPlayState = "paused";
     musicInfo.classList.remove("playSong");
     pauseActive.classList.remove("active");
     play.classList.remove("active");
     songAudio.pause();
-    // rotatingImg();
   });
 
   backward.addEventListener("click", () => {
     songIndex = (songIndex - 1 + allSongs.length) % allSongs.length;
     console.log(songIndex);
     songs(allSongs[songIndex]);
-    musicInfo.classList.add("playSong");
-    pauseActive.classList.toggle("active");
-    play.classList.toggle("active");
-    songAudio.play();
+
+    if (!play.classList.contains("active")) {
+      musicImg.classList.add("musicPlaying");
+       musicImg.style.webkitAnimationPlayState = "running";
+      pauseActive.classList.add("active");
+      play.classList.add("active");
+      songAudio.play();
+    }
   });
 
   forward.addEventListener("click", () => {
+    if (songIndex <= allSongs.length) {
+      songIndex = (songIndex + 1) % allSongs.length;
+      console.log(songIndex);
+      songs(allSongs[songIndex]);
+       if (!play.classList.contains("active")) {
+         musicImg.classList.add("musicPlaying");
+         musicImg.style.webkitAnimationPlayState = "running";
+         pauseActive.classList.add("active");
+         play.classList.add("active");
+         songAudio.play();
+       }
+    }
+  });
+
+  function updateProgress(e) {
+    // console.log(e.srcElement.currentTime)
+    // console.log(e.srcElement.duration)
+
+    let { currentTime, duration } = e.srcElement;
+    let progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+
+  function setProgress(e) {
+    let width = this.clientWidth;
+    let clickX = e.offsetX;
+    let duration = songAudio.duration;
+    songAudio.currentTime = (clickX / width) * duration;
+    // console.log(`width: ${width}`);
+    console.log(`clickX: ${clickX}`);
+  }
+
+  function nextSong() {
     if (songIndex <= allSongs.length) {
       songIndex = (songIndex + 1) % allSongs.length;
       console.log(songIndex);
@@ -70,41 +97,9 @@ document.addEventListener("DOMContentLoaded", () => {
       play.classList.toggle("active");
       songAudio.play();
     }
-  });
-
-
-  function updateProgress(e) {
-    // console.log(e.srcElement.currentTime)
-    // console.log(e.srcElement.duration)
-
-    let {currentTime,duration}=e.srcElement
-    let progressPercent=(currentTime/duration)*100
-    progress.style.width=`${progressPercent}%`;
   }
 
-  function setProgress(e) {
-    let width=this.clientWidth
-    let clickX=e.offsetX
-    let duration=songAudio.duration
-    songAudio.currentTime=(clickX/width)*duration
-    // console.log(`width: ${width}`);
-    console.log(`clickX: ${clickX}`);
-  }
-
-  function nextSong(){
-     if (songIndex <= allSongs.length) {
-       songIndex = (songIndex + 1) % allSongs.length;
-       console.log(songIndex);
-       songs(allSongs[songIndex]);
-       musicInfo.classList.add("playSong");
-       pauseActive.classList.toggle("active");
-       play.classList.toggle("active");
-       songAudio.play();
-     }
-  }
-
-
-songAudio.addEventListener("timeupdate", updateProgress);
-progressContainer.addEventListener('click',setProgress)
-songAudio.addEventListener('ended',nextSong)
+  songAudio.addEventListener("timeupdate", updateProgress);
+  progressContainer.addEventListener("click", setProgress);
+  songAudio.addEventListener("ended", nextSong);
 });
